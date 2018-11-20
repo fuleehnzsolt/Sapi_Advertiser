ac1917c34696299a2be468ab7878a3a1063311d4
fuleeh <fszili882@gmail.com>
2018-11-20 18:03:51 +0200

    Register and Login

--- app/src/main/java/com/example/fuleehnzsolt/sapi_advertiser/MainActivity.java
+++ app/src/main/java/com/example/fuleehnzsolt/sapi_advertiser/MainActivity.java
@@ -1,42 +1,43 @@
 package com.example.fuleehnzsolt.sapi_advertiser;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText lPhoneNumber, lCode;
     private Button lToRegisterButton, lGetCodeButton, loginButton;
-
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     private FirebaseAuth mAuth;
-
     private FirebaseFirestore mFirestore;
-
     private String phoneNumber;
     private String mVerificationId;
     private String verificationCode;
-
     private FirebaseFirestore db;
-
     private String passPhoneNumber, passFirstName, passLastName;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +47,11 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         mFirestore = FirebaseFirestore.getInstance();
-
-        lPhoneNumber = (EditText) findViewById(R.id.lPhoneNumber);
-        lCode = (EditText) findViewById(R.id.lCode);
-        lToRegisterButton = (Button) findViewById(R.id.lToRegisterButton);
-        loginButton = (Button) findViewById(R.id.loginButton);
-
+        lPhoneNumber = findViewById(R.id.lPhoneNumber);
+        lCode = findViewById(R.id.lCode);
+        lToRegisterButton = findViewById(R.id.lToRegisterButton);
+        loginButton = findViewById(R.id.loginButton);
+        lGetCodeButton = findViewById(R.id.lGetCodeButton);
         lCode.setVisibility(View.INVISIBLE);
         loginButton.setVisibility(View.INVISIBLE);
 
@@ -109,5 +109,67 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verificationCode = lCode.getText().toString();
+
+                if (TextUtils.isEmpty(verificationCode)) {
+                    Toast.makeText(MainActivity.this, "Enter the Code", Toast.LENGTH_SHORT).show();
+                } else {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verificationCode);
+                    signInWithPhoneAuthCredential(credential);
+                }
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(MainActivity.this, "You are logged in!", Toast.LENGTH_SHORT).show();
+                lCode.setVisibility(View.INVISIBLE);
+                loginButton.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                Toast.makeText(MainActivity.this, "Code sent...", Toast.LENGTH_SHORT).show();
+
+                lCode.setVisibility(View.VISIBLE);
+                loginButton.setVisibility(View.VISIBLE);
+            }
+        };
+
     }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Successfully logged in!", Toast.LENGTH_SHORT).show();
+
+                            finish();
+                        } else {
+
+                            String message = task.getException().toString();
+                            Toast.makeText(MainActivity.this, "Error: " + message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/fuleehnzsolt/sapi_advertiser/RegisterActivity.java
@@ -0,0 +1,212 @@
+package com.example.fuleehnzsolt.sapi_advertiser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import com.example.fuleehnzsolt.sapi_advertiser.Data.User;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText rFirstName, rLastName, rPhoneNumber, rCode;
+    private Button rCodeButton, registerButton;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private CollectionReference dbPhoneNumbers;
+    private String phoneNumber;
+    private String mVerificationId;
+    private String verificationCode;
+    private String firstName;
+    private String lastName;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        rFirstName = findViewById(R.id.rfName);
+        rLastName = findViewById(R.id.rlName);
+        rPhoneNumber = findViewById(R.id.rPhoneNumber);
+        rCode = findViewById(R.id.rCodeEdit);
+        rCodeButton = findViewById(R.id.rGetCodeButton);
+        registerButton = findViewById(R.id.regButton);
+
+        rCode.setVisibility(View.INVISIBLE);
+        registerButton.setVisibility(View.INVISIBLE);
+
+        rCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                phoneNumber = rPhoneNumber.getText().toString();
+                phoneNumber = "+40" + phoneNumber;
+                firstName = rFirstName.getText().toString();
+                lastName = rLastName.getText().toString();
+
+                if (TextUtils.isEmpty(phoneNumber)) {
+
+                    Toast.makeText(RegisterActivity.this, "Enter your phone number, please!", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    CollectionReference collectionReference = db.collection("users");
+                    Query query = collectionReference.whereEqualTo("phoneNumber", phoneNumber);
+
+                    query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            if (queryDocumentSnapshots.isEmpty()) {
+                                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                        phoneNumber,        // Phone number to verify
+                                        60,                 // Timeout duration
+                                        TimeUnit.SECONDS,   // Unit of timeout
+                                        RegisterActivity.this,               // Activity (for callback binding)
+                                        mCallbacks // OnVerificationStateChangedCallbacks
+                                );
+                            } else {
+
+                                Toast.makeText(RegisterActivity.this, "This number is already registered", Toast.LENGTH_LONG).show();
+
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                verificationCode = rCode.getText().toString();
+
+                if (TextUtils.isEmpty(verificationCode)) {
+
+                    Toast.makeText(RegisterActivity.this, "Enter the Code!", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    dbPhoneNumbers = db.collection("users");
+                    User user = new User(
+                            firstName,
+                            lastName,
+                            phoneNumber
+                    );
+
+                    dbPhoneNumbers.add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+
+                            Toast.makeText(RegisterActivity.this, "Account creation succeed!", Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verificationCode);
+                    sPhoneAuthCredential(credential);
+
+                }
+
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+
+                dbPhoneNumbers = db.collection("users");
+                User user = new User(
+                        firstName,
+                        lastName,
+                        phoneNumber
+                );
+
+                dbPhoneNumbers.add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(RegisterActivity.this, "Account creation succeed!", Toast.LENGTH_LONG).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_LONG);
+                    }
+                });
+
+
+                sPhoneAuthCredential(credential);
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+                Toast.makeText(RegisterActivity.this, "Verification failed!", Toast.LENGTH_LONG).show();
+                rCode.setVisibility(View.INVISIBLE);
+                registerButton.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                Toast.makeText(RegisterActivity.this, "Register code sent...", Toast.LENGTH_SHORT).show();
+
+                rCode.setVisibility(View.VISIBLE);
+                registerButton.setVisibility(View.VISIBLE);
+            }
+        };
+
+    }
+
+    private void sPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "You are logged in!", Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(RegisterActivity.this, .class));
+                            finish();
+                        } else {
+                            String message = task.getException().toString();
+                            Toast.makeText(RegisterActivity.this, "Error: " + message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
+
